04c8f2283c122533ff18ea00c651d8bea6be1514
Martin Mogusu <martinmogusu@gmail.com>
2018-11-01 11:22:36 +0300

    Added additional test cases

---
+++ django_daraja/mpesa/core.py
@@ -0,0 +1,12 @@
+class MpesaClient:
+	"""
+	This is the core MPESA client. 
+
+	The Mpesa Client will access all interactions with the MPESA Daraja API.
+	"""
+
+	def __init__(self):
+		"""
+		The constructor for MpesaClient class
+		"""
+		pass
---
+++ tests/test_client.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Test the MPESA client
+"""
+
+from __future__ import unicode_literals
+from django.urls import reverse
+from django.test import TestCase
+from django_daraja.mpesa.core import MpesaClient
+
+class MpesaClientTestCase(TestCase):
+
+	def test_client_init(self):
+		cl = MpesaClient()
+		self.assertEqual(cl.__class__.__name__, 'MpesaClient')

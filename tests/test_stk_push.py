86ca6d5d49ae15f1313f08e92ff85b256d6eee35
Martin Mogusu <martinmogusu@gmail.com>
2018-11-20 15:42:46 +0300

    Removed multiple STK push test cases - avoid spike arrest violation

--- tests/test_stk_push.py
+++ tests/test_stk_push.py
@@ -24,16 +24,4 @@ class MpesaStkPushTestCase(TestCase):
 		response = self.cl.stk_push(phone_number, amount, account_reference, transaction_desc, callback_url)
 		self.assertEqual(response.response_description, 'Success. Request accepted for processing')
 
-	def test_stk_push_invalid_phone_number(self):
-		'''
-		Test STK push with invalid phone number
-		'''
-
-		phone_number = '254733000'
-		amount = 1
-		account_reference = 'reference'
-		transaction_desc = 'Description'
-		callback_url = 'https://darajambili.herokuapp.com/express-payment'
-		response = self.cl.stk_push(phone_number, amount, account_reference, transaction_desc, callback_url)
-		self.assertEqual(response.status_code, 500)
 		
